2ed4f7b5554a13c251f73888d56b2059ca381bdd
PhilMakzoume <phil@polarstork.com>
2020-07-12 16:39:22 +0300

    Revert MultiStatement system, to do handle this in route as middleware

--- src/Models/unicorns.model.js
+++ src/Models/unicorns.model.js
@@ -6,82 +6,63 @@ export const unicorns_model = {
   transactions: [
     {
       func: "getUnicorn",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.SELECT,
-          stmtKey: "UNICORN_SEL_ID",
-          query: `SELECT * FROM unicorns WHERE unicorn_id = ?`,
-          pk: "unicorn_id",
-          expectedProps: [],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.SELECT,
+        stmtKey: "UNICORN_SEL_ID",
+        query: `SELECT * FROM unicorns WHERE unicorn_id = ?`,
+        pk: "unicorn_id",
+        expectedProps: [],
+      },
     },
     {
       func: "getAllUnicorns",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.SELECT_ALL,
-          stmtKey: "UNICORN_SEL_ALL",
-          query: `SELECT * FROM unicorns`,
-          pk: "",
-          expectedProps: [],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.SELECT_ALL,
+        stmtKey: "UNICORN_SEL_ALL",
+        query: `SELECT * FROM unicorns`,
+        pk: "",
+        expectedProps: [],
+      },
     },
     {
       func: "createUnicorn",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.INSERT,
-          stmtKey: "UNICORN_INS",
-          query: `INSERT INTO unicorns (name, age, color) VALUES ($name, $age, $color)`,
-          pk: "",
-          expectedProps: ["name", "age", "color"],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.INSERT,
+        stmtKey: "UNICORN_INS",
+        query: `INSERT INTO unicorns (name, age, color) VALUES ($name, $age, $color)`,
+        pk: "",
+        expectedProps: ["name", "age", "color"],
+      },
     },
     {
       func: "updateUnicorn",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.UPDATE,
-          stmtKey: "UNICORN_UPD",
-          query: `UPDATE unicorns SET name = $name, age = $age, color = $color WHERE unicorn_id = @id`,
-          pk: "unicorn_id",
-          expectedProps: ["name", "age", "color"],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.UPDATE,
+        stmtKey: "UNICORN_UPD",
+        query: `UPDATE unicorns SET name = $name, age = $age, color = $color WHERE unicorn_id = @id`,
+        pk: "unicorn_id",
+        expectedProps: ["name", "age", "color"],
+      },
     },
     {
       func: "deleteUnicorn",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.DELETE,
-          stmtKey: "UNICORN_DEL",
-          query: `DELETE FROM unicorns WHERE unicorn_id = ?`,
-          pk: "unicorn_id",
-          expectedProps: [],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.DELETE,
+        stmtKey: "UNICORN_DEL",
+        query: `DELETE FROM unicorns WHERE unicorn_id = ?`,
+        pk: "unicorn_id",
+        expectedProps: [],
+      },
     },
     {
       func: "resetUnicorns",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.DELETE_ALL,
-          stmtKey: "UNICORN_DEL_ALL",
-          query: `DELETE FROM unicorns`,
-          pk: "",
-          expectedProps: [],
-        },
-        {
-          type: ENUM_QUERY_TYPES.INSERT,
-          stmtKey: "UNICORN_INS_MANY",
-          query: `INSERT INTO unicorns (name, age, color) VALUES ($name, $age, $color)`,
-          pk: "",
-          expectedProps: ["name", "age", "color"],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.DELETE_ALL,
+        stmtKey: "UNICORN_DEL_ALL",
+        query: `DELETE FROM unicorns`,
+        pk: "",
+        expectedProps: [],
+      },
     },
   ],
   routes: [
--- src/Models/users.model.js
+++ src/Models/users.model.js
@@ -6,100 +6,73 @@ export const users_model = {
   transactions: [
     {
       func: "getUser",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.SELECT,
-          stmtKey: "USER_SEL_ID",
-          query: `SELECT * FROM users WHERE user_id = ?`,
-          pk: "user_id",
-          expectedProps: [],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.SELECT,
+        stmtKey: "USER_SEL_ID",
+        query: `SELECT * FROM users WHERE user_id = ?`,
+        pk: "user_id",
+        expectedProps: [],
+      },
     },
     {
       func: "getAllUsers",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.SELECT_ALL,
-          stmtKey: "USER_SEL_ALL",
-          query: `SELECT * FROM users`,
-          pk: "",
-          expectedProps: [],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.SELECT_ALL,
+        stmtKey: "USER_SEL_ALL",
+        query: `SELECT * FROM users`,
+        pk: "",
+        expectedProps: [],
+      },
     },
     {
       func: "createUser",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.INSERT,
-          stmtKey: "USER_INS",
-          query: `INSERT INTO users (first_name, last_name, email) VALUES ($first_name, $last_name, $email)`,
-          pk: "",
-          expectedProps: ["first_name", "last_name", "email"],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.INSERT,
+        stmtKey: "USER_INS",
+        query: `INSERT INTO users (first_name, last_name, email) VALUES ($first_name, $last_name, $email)`,
+        pk: "",
+        expectedProps: ["first_name", "last_name", "email"],
+      },
     },
     {
       func: "updateUser",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.UPDATE,
-          stmtKey: "USER_UPD",
-          query: `UPDATE users SET first_name = $first_name, last_name = $last_name, email = $email WHERE user_id = @id`,
-          pk: "user_id",
-          expectedProps: ["first_name", "last_name", "email"],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.UPDATE,
+        stmtKey: "USER_UPD",
+        query: `UPDATE users SET first_name = $first_name, last_name = $last_name, email = $email WHERE user_id = @id`,
+        pk: "user_id",
+        expectedProps: ["first_name", "last_name", "email"],
+      },
     },
     {
       func: "deleteUser",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.DELETE,
-          stmtKey: "USER_DEL",
-          query: `DELETE FROM users WHERE user_id = ?`,
-          pk: "user_id",
-          expectedProps: [],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.DELETE,
+        stmtKey: "USER_DEL",
+        query: `DELETE FROM users WHERE user_id = ?`,
+        pk: "user_id",
+        expectedProps: [],
+      },
     },
     {
       func: "resetUser",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.DELETE_ALL,
-          stmtKey: "USER_DEL_ALL",
-          query: `DELETE FROM users`,
-          pk: "",
-          expectedProps: [],
-        },
-      ],
-    },
-    {
-      // TODO: Duplicate
-      func: "insertDefaultUsers",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.INSERT,
-          stmtKey: "USER_INS",
-          query: `INSERT INTO users (first_name, last_name, email) VALUES ($first_name, $last_name, $email)`,
-          pk: "",
-          expectedProps: ["first_name", "last_name", "email"],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.DELETE_ALL,
+        stmtKey: "USER_DEL_ALL",
+        query: `DELETE FROM users`,
+        pk: "",
+        expectedProps: [],
+      },
     },
     {
       func: "updateMany",
-      statements: [
-        {
-          type: ENUM_QUERY_TYPES.UPDATE,
-          stmtKey: "USER_UPD_MANY",
-          query: `UPDATE users SET first_name = $first_name, last_name = $last_name, email = $email WHERE user_id = @id`,
-          pk: "user_id",
-          expectedProps: ["first_name", "last_name", "email"],
-        },
-      ],
+      statement: {
+        type: ENUM_QUERY_TYPES.UPDATE,
+        stmtKey: "USER_UPD_MANY",
+        query: `UPDATE users SET first_name = $first_name, last_name = $last_name, email = $email WHERE user_id = @id`,
+        pk: "user_id",
+        expectedProps: ["first_name", "last_name", "email"],
+      },
     },
   ],
   routes: [
